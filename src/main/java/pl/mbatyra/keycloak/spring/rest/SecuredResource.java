f13d5b684bc669400296b42d58ae1b02f850284b
Mariusz Batyra <mbatyra@wp.eu>
2016-04-26 20:33:23 +0200

    Information from JWT about logged user

--- src/main/java/pl/mbatyra/keycloak/spring/rest/SecuredResource.java
+++ src/main/java/pl/mbatyra/keycloak/spring/rest/SecuredResource.java
@@ -2,6 +2,9 @@ package pl.mbatyra.keycloak.spring.rest;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.adapters.RefreshableKeycloakSecurityContext;
+import org.keycloak.representations.AccessToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +20,17 @@ public class SecuredResource {
     private static final Logger LOG = Logger.getLogger(SecuredResource.class.getName());
 
     @RequestMapping(value = "test", method = RequestMethod.GET)
-    public String getTest() {
+    public String getTest(KeycloakPrincipal<RefreshableKeycloakSecurityContext> principal) {
         LOG.log(Level.INFO, "Access to secured resource granted!");
-        return "Content of secured resource";
+
+        AccessToken accessToken = principal.getKeycloakSecurityContext().getToken();
+        String issuer = accessToken.getIssuer();
+        String username = accessToken.getPreferredUsername();
+
+        LOG.log(Level.INFO, "Token Issuer: {0}", issuer);
+        LOG.log(Level.INFO, "username: {0}", username);
+
+        return "Content of secured resource. Username: " + username;
     }
 
 }
